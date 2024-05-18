0cf2b2a32db0aa2e28be1c5acb5b45c47c0928c3
anyhhope <hopeanysimova@yandex.ru>
2024-05-19 02:12:03 +0300

    add model backend

--- back/app/main2.py
+++ back/app/main2.py
@@ -11,6 +11,15 @@ import io
 import json
 import mimetypes
 from fastapi.responses import StreamingResponse
+import torch
+from tqdm import tqdm
+import os
+from torch.utils.data import  DataLoader, Dataset
+import numpy as np
+from torch.nn import functional as F
+from torchvision import transforms
+import pandas as pd
+
 
 mimetypes.init()
 
@@ -25,7 +34,33 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-yolo_model = YOLO("yolo-first.pt")
+
+def get_model_resnet(path_weights_resnet):
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+#     model = models.resnet152(pretrained=False)
+    with open(path_weights_resnet, 'rb') as f:
+        model = torch.load(path_weights_resnet, map_location=device)
+    model = model.to(device)
+    return model
+
+def get_model_vit(path_weights_vit):
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'    
+    with open(path_weights_vit, 'rb') as f:
+        model = torch.load(path_weights_vit, map_location=device)
+    model = model.to(device)
+    return model
+
+def get_model_effnet(path_weights_effnet):
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'    
+    with open(path_weights_effnet, 'rb') as f:
+        model = torch.load(path_weights_effnet, map_location=device)
+    model = model.to(device)
+    return model
+
+model_yolo = YOLO("yolov8_best.pt", task='detect')
+model_resnet = get_model_resnet("resnet_crop_best.pth")
+model_vit = get_model_vit("vit_small_model_full.pth")
+
 
 # templates = Jinja2Templates(directory="./ui")
 # app.mount("/assets", StaticFiles(directory="./ui/assets"), "assets")
@@ -42,12 +77,16 @@ yolo_model = YOLO("yolo-first.pt")
 
 @app.post("/upload")
 async def upload(files: List[UploadFile] = []):
+
     if not files:
         raise HTTPException(status_code=400, detail="No files provided")
 
     class_images = {0: [], 1: [], 2: []}
     class_counts = {0: 0, 1: 0, 2: 0}
     metadata = []
+
+    imagesList = []
+    imageByteList = []
 
     for file in files:
         if file.filename.endswith('.zip'):
@@ -57,27 +96,38 @@ async def upload(files: List[UploadFile] = []):
                         with zip_ref.open(zip_info) as image_file:
                             file_bytes = await image_file.read()
                             img = Image.open(io.BytesIO(file_bytes))
-                            class_label = detect_img_with_yolo(img)
-                            class_images[class_label].append((zip_info.filename, file_bytes))
-                            class_counts[class_label] += 1
+                            imagesList.append(img)
+                            imageByteList.append((zip_info.filename, file_bytes))
+                            # class_label = detect_img_with_yolo(img)
+                            # class_images[class_label].append((zip_info.filename, file_bytes))
+                            # class_counts[class_label] += 1
                             metadata.append({
                                 'filename': zip_info.filename,
                                 'width': img.width,
                                 'height': img.height,
-                                'class': class_label
+                                # 'class': class_label
                             })
         else:
             file_bytes = await file.read()
             img = Image.open(io.BytesIO(file_bytes))
-            class_label = detect_img_with_yolo(img)
-            class_images[class_label].append((file.filename, file_bytes))
-            class_counts[class_label] += 1
+            imagesList.append(img)
+            imageByteList.append((file.filename, file_bytes))
+            # class_label = detect_img_with_yolo(img)
+            # class_images[class_label].append((file.filename, file_bytes))
+            # class_counts[class_label] += 1
             metadata.append({
                 'filename': file.filename,
                 'width': img.width,
                 'height': img.height,
-                'class': class_label
+                # 'class': class_label
             })
+
+    predicted_results = make_predict(imagesList)
+ 
+    for i, pred_class in enumerate(predicted_results):
+        class_images[pred_class].append(imageByteList[i])
+        class_counts[pred_class] += 1
+
 
     zip_files = create_zip_files(class_images)
     stats = {
@@ -94,9 +144,160 @@ async def upload(files: List[UploadFile] = []):
 
 
 def detect_img_with_yolo(img):
-    results = yolo_model([img], device="cpu")
+    results = model_yolo([img], device="cpu")
     for res in results:
         return int(res.boxes.cls.mode()[0])
+
+def predict_model_yolo_image(model, img_array):
+    results = []
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'    
+    with torch.no_grad():
+        for image in img_array:
+            predict = model(image, save=False, stream=True, verbose=False, device=device)
+            for res in predict:
+                results.append(res)
+    return results
+
+def predict_model_yolo(model, path_data):
+    results = []
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    with torch.no_grad():
+        for photo_name in tqdm(os.listdir(path_data)):
+            if photo_name[-3:] == 'csv':
+                continue
+            predict = model([path_data + '/' + photo_name],
+                      save=False, stream=True, verbose=False, device=device)
+            for res in predict:
+                results.append(res)
+    return results
+
+def predict_model_classification(model, dataloader, name=""):
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    model.eval()
+    pred, pred_path, pred_proba = [], [], []
+    for x_batch, x_path in dataloader:
+        x_batch = x_batch.to(device)
+        if name == "vit":
+            outp = model(x_batch).logits
+        else:
+            outp = model(x_batch)
+        proba = F.softmax(outp, dim=1)
+        pred_path += list(x_path)
+        pred += list(outp.cpu().detach().numpy().argmax(1))
+        pred_proba += list(proba.cpu().detach().numpy())
+    
+    return pred, pred_path, pred_proba
+
+def crop_photo_inplace(results_from_model_yolo, padding_percent=0.4):
+    for i, result in enumerate(results_from_model_yolo):
+        area_detect, box_detect = 0, []
+        for box in result.boxes.xyxy:
+            x1, y1, x2, y2 = box
+            if (x2 - x1) * (y2 - y1) > area_detect:
+                box_detect = box
+        if len(box_detect):
+            x1, y1, x2, y2 = map(int, box_detect)
+            if padding_percent > 0:
+                pad_x, pad_y = (x2 - x1) * padding_percent / 2, (y2 - y1) * padding_percent / 2
+                x1 = max(0, x1 - pad_x)
+                x2 = min(result.orig_img.shape[1], x2 + pad_x)
+                y1 = max(0, y1 - pad_y)
+                y2 = min(result.orig_img.shape[0], y2 + pad_y)
+                x1, y1, x2, y2 = map(int, [x1, y1, x2, y2])
+            results_from_model_yolo[i].orig_img = result.orig_img[y1:y2, x1:x2]
+    return results_from_model_yolo 
+
+
+class TestDataset(Dataset):
+    def __init__(self, result, transform=None):
+        self.result = result
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.result)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+            
+        img_name = self.result[idx].orig_img
+        path = self.result[idx].path
+        
+        if self.transform:
+            img_name = self.transform(img_name)
+
+        return img_name, path
+    
+def yolo_step(data):
+    if isinstance(data, str):
+        res = predict_model_yolo(model_yolo, data)
+    else:
+        res = predict_model_yolo_image(model_yolo, data)
+    crop_photo_inplace(res, 0.8)
+    return res
+    
+def resnet_step(dataloader):
+    predict, path, proba = predict_model_classification(model_resnet, dataloader)
+    return predict, path, proba
+
+def vit_step(dataloader):
+    predict, path, proba = predict_model_classification(model_vit, dataloader, name='vit')
+    return predict, path, proba
+
+def effnet_step(path_effnet, dataloader):
+    model_effnet = get_model_effnet(path_effnet)
+    predict, path, proba = predict_model_classification(model_effnet, dataloader)
+    return predict, path, proba
+
+def make_mean(result):
+    col = result.columns
+    proba_col = col[col.str.startswith('proba')]
+    
+    def mean_proba(row):
+        return row[proba_col].mean()
+    result['mean_proba'] = result.apply(mean_proba,axis=1)
+    result['pred_mean'] = result['mean_proba'].map(lambda x: x.argmax())
+    
+    return result
+
+def make_predict(data,         # путь до папки с фото или фото
+                 path_resnet=None,
+                 path_vit=None,
+                 path_effnet=None):
+    
+    data_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((224, 224)),
+    ])
+    
+    result = yolo_step(data)
+    test_dataset = TestDataset(result, data_transforms)
+    test_daloader = DataLoader(test_dataset, batch_size=4, shuffle=False)
+    
+    predict = pd.DataFrame()
+    predict['pred_yolo'] = [photo.boxes.cls.mode()[0].item() if photo.boxes.cls.numel() != 0 else np.nan for photo in result]
+                   
+
+    predict_resnet, path_resnet, proba_resnet = resnet_step(test_daloader)
+    predict['pred_resnet'] = predict_resnet
+    predict['path_resnet'] = path_resnet
+    predict['proba_resnet'] = proba_resnet
+    
+
+    predict_vit, path_vit, proba_vit = vit_step(test_daloader)
+    predict['pred_vit'] = predict_vit
+    predict['path_vit'] = path_vit
+    predict['proba_vit'] = proba_vit
+        
+    if path_effnet:
+        predict_effnet, path_effnet, proba_effnet = effnet_step(path_effnet, test_daloader)
+        predict['pred_effnet'] = predict_effnet
+        predict['path_effnet'] = path_effnet
+        predict['proba_effnet'] = proba_effnet
+        
+    predict = make_mean(predict)
+    
+    return predict['pred_mean'].values
 
 
 def create_zip_files(class_images):
@@ -133,3 +334,4 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
+    
--- frontend/src/components/widgets/ImageUploader.tsx
+++ frontend/src/components/widgets/ImageUploader.tsx
@@ -3,10 +3,9 @@ import { UploadOutlined } from "@ant-design/icons";
 import { Button, message, Upload } from "antd";
 import type { GetProp, UploadFile, UploadProps } from "antd";
 import { IClassRes } from "../../models/index";
-import JSZip, { file } from "jszip";
+import JSZip from "jszip";
 import { saveAs } from "file-saver";
 import { API_HOST } from "../../constant";
-
 
 type FileType = Parameters<GetProp<UploadProps, "beforeUpload">>[0];
 
@@ -16,6 +15,7 @@ interface ImageUploaderProps {
 
 const ImageUploader: React.FC<ImageUploaderProps> = ({ onClassRes }) => {
   const [fileList, setFileList] = useState<UploadFile[]>([]);
+  // const [dirPath, setDirPath] = useState<UploadFile>();
   const [uploading, setUploading] = useState(false);
   const abortController = useRef<AbortController | null>(null);
 
@@ -25,7 +25,7 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ onClassRes }) => {
       formdata.append("files", file as FileType);
     });
 
-    console.log(fileList)
+    console.log(fileList);
 
     setUploading(true);
     abortController.current = new AbortController();
@@ -40,18 +40,18 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ onClassRes }) => {
 
       if (response.ok) {
         const blob = await response.blob();
-      
+
         saveAs(blob, "classified_images_with_statistics.zip");
-  
+
         const zip = await JSZip.loadAsync(blob);
         const metadataFile = zip.file("statistics.json");
         if (metadataFile) {
           const metadataContent = await metadataFile.async("string");
           const result = JSON.parse(metadataContent);
           console.log(result);
-          onClassRes(result);  
-        } 
-  
+          onClassRes(result);
+        }
+
         setFileList([]);
       }
     } catch (error) {
@@ -60,11 +60,14 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ onClassRes }) => {
       } else {
         message.error("Ошибка. Попробуйте снова");
       }
-    }
-    finally {
+    } finally {
       setUploading(false);
     }
   }
+
+  // async function handleUploadDir() {
+  //   console.log(dirPath.path)
+  // }
 
   // const handleCancel = () => {
   //   if (abortController.current) {
@@ -97,10 +100,24 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ onClassRes }) => {
     fileList,
   };
 
+  // const propsDir: UploadProps = {
+  //   beforeUpload: (file) => {
+  //     if (!dirPath) {
+  //       setDirPath(file);
+  //     }
+  //     return false;
+  //   },
+  //   showUploadList: false,
+  //   directory: true,
+  //   multiple: true,
+  // };
+
   const buttonStyles = {
     marginTop: 16,
     marginRight: 8,
-    ...(fileList.length === 0 || uploading ? { color: 'black', backgroundColor: '#f5f5f5', borderColor: '#AF91C1' } : {}),
+    ...(fileList.length === 0 || uploading
+      ? { color: "black", backgroundColor: "#f5f5f5", borderColor: "#AF91C1" }
+      : {}),
   };
 
   return (
@@ -110,7 +127,7 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ onClassRes }) => {
         width: "400px",
         border: "2px dashed lightblue",
         borderRadius: "10px",
-        minHeight: '100px'
+        minHeight: "100px",
       }}
     >
       <p style={{ marginBottom: "10px" }}>
@@ -140,6 +157,17 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ onClassRes }) => {
       >
         {uploading ? "Происходит классификация" : "Начать загрузку"}
       </Button>
+      {/* <Upload {...propsDir} directory>
+        <Button icon={<UploadOutlined />}>Upload Directory</Button>
+      </Upload>
+      <Button
+        type="primary"
+        onClick={handleUploadDir}
+        loading={uploading}
+        style={buttonStyles}
+      >
+        {uploading ? "Происходит классификация" : "Начать загрузку"}
+      </Button> */}
       {/* <Button
         type="default"
         onClick={handleCancel}
